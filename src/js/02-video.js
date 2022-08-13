a5c719a8d9a1afe071709b527d2c4576c14226f7
Ulyana Kalinichenko <ulyakalinichenko1990@gmail.com>
2022-08-14 01:14:48 +0300

    02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+/*import Player from "@vimeo/player";
+import { throttle } from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+const throttle = e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds)};
+
+player.on('timeupdate', throttle);*/
+
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle'
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+    
+
+function findCurrentTime (data) 
+        {localStorage.setItem('videoplayer-current-time', data.seconds);}
+
+        player.on('timeupdate', throttle(findCurrentTime, 1000));
+
+
+
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+
+player.setCurrentTime(getCurrentTime)
+    .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+   
