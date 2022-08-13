f459ac1776bb7ccc8f0e723d13b0772bc95354fa
Ulyana Kalinichenko <ulyakalinichenko1990@gmail.com>
2022-08-14 01:15:42 +0300

    02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,3 @@
-/*import Player from "@vimeo/player";
-import { throttle } from 'lodash.throttle';
-
-const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
-
-
-const throttle = e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds)};
-
-player.on('timeupdate', throttle);*/
-
 import Player from "@vimeo/player";
 import throttle from 'lodash.throttle'
 
