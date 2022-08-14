1de7414979982dfe914755904ac134d37135f868
Ulyana Kalinichenko <ulyakalinichenko1990@gmail.com>
2022-08-14 17:13:38 +0300

    03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,53 +1,38 @@
-import throttle from 'lodash.throttle';
-import '../css/common.css';
-import '../css/03-feedback.css';
+import throttle from "lodash.throttle";
 
 
-const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.jfeedback-form, textarea'),
+form.addEventListener('input', throttle(onTextareaInput),500);
+form.addEventListener('submit', onFormSubmit);
+
+
+ReturnDataLocalStorage();
+
+function onTextareaInput(event) {
+    event.preventDefault()
+    const emailData = form.email.value
+    const textData = form.message.value
+   
+    const DataInfo = {emailData,textData};
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(DataInfo));
 };
-    
 
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 2000));
+function onFormSubmit (event) {
+    event.preventDefault();
+const DataForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+localStorage.removeItem('feedback-form-state');
+    form.reset();
+    return;
+};
+  
 
-populateTextarea();
-
-function onTextareaInput(evt) {
-    evt.preventDefault()
-  const message = evt.target.value;
-
-    localStorage.setItem(STORAGE_KEY, message);
-    
+function ReturnDataLocalStorage() {
+    const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (storageData) {
+        form.email.value = storageData.emailData;
+        form.message.value = storageData.textData;
+    }
 }
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-
-  console.log('Отправляем форму');
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-  if (savedMessage) {
-    refs.textarea.value = savedMessage;
-  }
-}
-
-const formData = {};
-
-refs.form.addEventListener('input', e => {
- console.log(e.target.name);
-console.log(e.target.value);
-
-formData[e.target.name] = e.target.value;
-
-console.log(formData);
-});
